24d5131b34289b3f2c93edc41866bdf49608f08f
ozielms <ozielmaster@gmail.com>
2022-05-06 00:17:04 -0300

    Extração dia e mes da data de entrada

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,40 @@
-function cadContas(cartao, descricao, valorCompra, numParcelas) {
-  var tb = document.getElementById('tbGastos') /* Pegar a table */
-  var qtdLinhas = tb.rows.length /* Ver quantas linhas tem a tabela */
-  var linha = tb.insertRow(qtdLinhas) /* Inserir uma linha */
+function cadContas(cartao, descricao, valorCompra, dataEntrada, numParcelas) {
+  var tb = document.getElementById('tbGastos'); /* Pegar a table */
+  var qtdLinhas = tb.rows.length; /* Ver quantas linhas tem a tabela */
+  var linha = tb.insertRow(qtdLinhas); /* Inserir uma linha */
 
+  /* Extrair dia e mês da data */
+  var data = new Date(dataEntrada);
+  console.log(data);
+  var mes = data.toLocaleString('default', { month: 'long' });
+  var dia = data.getUTCDate();
+  
   /* Inserir colunas na tabela */
-  var cellCodigo = linha.insertCell(0)
-  var cellcartao = linha.insertCell(1)
-  var cellDescricao = linha.insertCell(2)
-  var cellValorCompra = linha.insertCell(3)
-  var cellNumParcelas = linha.insertCell(4)
-  var cellValorDaParcela = linha.insertCell(5)
+  var cellCodigo = linha.insertCell(0);
+  var cellDia = linha.insertCell(1);
+  var cellcartao = linha.insertCell(2);
+  var cellDescricao = linha.insertCell(3);
+  var cellValorCompra = linha.insertCell(4);
+  var cellNumParcelas = linha.insertCell(5);
+  var cellValorDaParcela = linha.insertCell(6);
 
-  valorParcela = parseFloat(valorCompra) / parseFloat(numParcelas)
+  valorParcela = parseFloat(valorCompra) / parseFloat(numParcelas);
 
   /* Inserir dados no HTML */
-  cellCodigo.innerHTML = qtdLinhas
-  cellcartao.innerHTML = cartao
-  cellDescricao.innerHTML = descricao
-  cellValorCompra.innerHTML = parseFloat(valorCompra).toFixed(2)
-  cellNumParcelas.innerHTML = numParcelas
-  cellValorDaParcela.innerHTML = valorParcela.toFixed(2)
+  cellCodigo.innerHTML = qtdLinhas;
+  cellDia.innerHTML = dia;
+  cellcartao.innerHTML = cartao;
+  cellDescricao.innerHTML = descricao;
+  cellValorCompra.innerHTML = parseFloat(valorCompra).toFixed(2);
+  cellNumParcelas.innerHTML = numParcelas;
+  cellValorDaParcela.innerHTML = valorParcela.toFixed(2);
 
   /* CAPTURAR VALOR DO TOTAL E DEVOLVER SOMADO*/
-  var f = document.querySelector('.celTotalJan')
-  var capturado = f.value
+  var f = document.querySelector('.celTotalJan');
+  var capturado = f.value;
   if (capturado == '') {
-    capturado = 0
+    capturado = 0;
   }
-  var temp = parseFloat(capturado) + parseFloat(valorParcela)
-  f.value = temp.toFixed(2)
+  var temp = parseFloat(capturado) + parseFloat(valorParcela);
+  f.value = temp.toFixed(2);
 }
